ba6dfc3565b09c40551e453c2465cbe9dcc147f8
Sherlyd <110980522+Sherlyd@users.noreply.github.com>
2023-10-30 13:51:58 -0300

    corregidas las rutas

--- ejerciciosTP2/transporte.py
+++ ejerciciosTP2/transporte.py
@@ -32,32 +32,6 @@ class Grafo:
     def agregar_movimiento(self, ciudad_origen, ciudad_destino, peso, precio):
         self.vertices[ciudad_origen].agregar_vecino(ciudad_destino, peso, precio)
 
-# Variable grafo contiene los datos del archivo ruta, usa la clase Grafo
-grafo = Grafo()
-ruta_del_archivo = '/home/dany/Escritorio/Nueva carpeta/algoritmos/TP-AyED/ejerciciosTP2/rutas.txt'
-
-try:
-    # Leo el archivo y guardo cada variable
-    with open(ruta_del_archivo, 'r') as arch_ruta:
-        for linea in arch_ruta:
-            datos = linea.strip().split(',')
-            ciudad_origen = datos[0]
-            ciudad_destino = datos[1]
-            peso = int(datos[2])
-            precio = int(datos[3])
-            # Me aseguro de que todas las rutas hayan sido agregadas, por si alguna no se agregó al inicio
-            if ciudad_origen not in grafo.vertices:
-                grafo.agregar_vertice(ciudad_origen)
-            if ciudad_destino not in grafo.vertices:
-                grafo.agregar_vertice(ciudad_destino)
-
-            grafo.agregar_movimiento(ciudad_origen, ciudad_destino, peso, precio)
-
-except FileNotFoundError:
-    print("El archivo no se encuentra.")
-except Exception as error:
-    print(f"Error: {error}")
-
 def buscar_precio_minimo(vertice, max_cuello_botella):
     """Función auxiliar para buscar el precio mínimo recursivamente."""
     if not vertice.vecinos:
@@ -66,27 +40,62 @@ def buscar_precio_minimo(vertice, max_cuello_botella):
     vertice.visitado = True
     precio_min = float('inf')
     for movimiento in vertice.vecinos:
-        ciudad_destino_objeto = grafo.vertices[movimiento.ciudad_destino]  # Obtén el objeto Vertice correspondiente a la ciudad de destino
+        ciudad_destino_objeto = grafo.vertices[movimiento.ciudad_destino]
         if not ciudad_destino_objeto.visitado and movimiento.peso_mx <= max_cuello_botella:
             precio_ruta_actual = movimiento.precio
             precio_min = min(precio_min, precio_ruta_actual)
     return precio_min
 
-def encontrar_cuello_botella(grafo, ciudad_origen):
+def encontrar_cuello_botella(grafo, ciudad_origen, ciudad_destino):
     """Determina el cuello de botella desde la ciudad de origen a la destino en el grafo."""
     max_cuello_botella = float('inf')
     for movimiento in grafo.vertices[ciudad_origen].vecinos:
-        max_cuello_botella = min(max_cuello_botella, movimiento.peso_mx)
+        if movimiento.ciudad_destino == ciudad_destino:
+            max_cuello_botella = min(max_cuello_botella, movimiento.peso_mx)
     return max_cuello_botella
 
-def precio_minimo(grafo, ciudad_origen):
-    """Determina el precio mínimo para transportar bienes desde la ciudad de origen hasta cualquier destino."""
-    max_cuello_botella = encontrar_cuello_botella(grafo, ciudad_origen)
-    return buscar_precio_minimo(grafo.vertices[ciudad_origen], max_cuello_botella)
+def encontrar_destinos_diferentes(grafo, ciudad_origen):
+    """Encuentra todos los destinos diferentes desde la ciudad de origen."""
+    destinos = set()
+    for movimiento in grafo.vertices[ciudad_origen].vecinos:
+        destinos.add(movimiento.ciudad_destino)
+    return list(destinos)
 
-# Prueba de que anda lo solicitado
+def calcular_precio_y_cuello_botella(grafo, ciudad_origen, ciudad_destino):
+    """Calcula el precio mínimo y el cuello de botella para un destino específico."""
+    max_cuello_botella = encontrar_cuello_botella(grafo, ciudad_origen, ciudad_destino)
+    precio_min = buscar_precio_minimo(grafo.vertices[ciudad_origen], max_cuello_botella)
+    return precio_min, max_cuello_botella
+
+# Crear el grafo y cargar datos desde el archivo
+grafo = Grafo()
+ruta_del_archivo = '/home/dany/Escritorio/Nueva carpeta/algoritmos/TP-AyED/ejerciciosTP2/rutas.txt'
+
+try:
+    with open(ruta_del_archivo, 'r') as arch_ruta:
+        for linea in arch_ruta:
+            datos = linea.strip().split(',')
+            ciudad_origen = datos[0]
+            ciudad_destino = datos[1]
+            peso = int(datos[2])
+            precio = int(datos[3])
+            if ciudad_origen not in grafo.vertices:
+                grafo.agregar_vertice(ciudad_origen)
+            if ciudad_destino not in grafo.vertices:
+                grafo.agregar_vertice(ciudad_destino)
+            grafo.agregar_movimiento(ciudad_origen, ciudad_destino, peso, precio)
+
+except FileNotFoundError:
+    print("El archivo no se encuentra.")
+except Exception as error:
+    print(f"Error: {error}")
+
 ciudad_origen = "CiudadBs.As."
-precio_min = precio_minimo(grafo, ciudad_origen)
-cuello_botella = encontrar_cuello_botella(grafo, ciudad_origen)
-print(f"Precio mínimo para transportar desde {ciudad_origen} hasta cualquier destino: {precio_min} unidades de 1000.")
-print(f"Peso máximo que se puede transportar desde {ciudad_origen} a cualquier otra ciudad de destino: {cuello_botella} kg.")
+destinos = encontrar_destinos_diferentes(grafo, ciudad_origen)
+
+for destino in destinos:
+    precio_min, cuello_botella = calcular_precio_y_cuello_botella(grafo, ciudad_origen, destino)
+    print(f"Para llegar a {destino}:")
+    print(f"Precio mínimo: {precio_min} unidades de 1000.")
+    print(f"Peso máximo que se puede transportar: {cuello_botella} kg.")
+    print("-----")
