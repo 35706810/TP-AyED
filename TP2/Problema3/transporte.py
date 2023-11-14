70ac36bce4342f1ab7a4b4aa437dc5794f610a71
Sherlyd <110980522+Sherlyd@users.noreply.github.com>
2023-11-14 00:14:13 -0300

    Update transporte.py

--- TP2/Problema3/transporte.py
+++ TP2/Problema3/transporte.py
@@ -22,23 +22,25 @@ class Vertice:
         self.predecesor_peso = None
 
     def agregar_vecino(self, ciudad_destino, peso, precio):
-        movimiento = Movimiento(self.id, ciudad_destino, peso, precio)
-        self.vecinos[ciudad_destino] = movimiento
-
-    def __str__(self):
-        return str(self.id) + ' conectadoA: ' + str([x for x in self.vecinos.keys()])
-
-    def obtener_conexiones(self):
-        return self.vecinos.keys()
-
-    def obtener_id(self):
-        return self.id
+        if ciudad_destino in self.vecinos:
+            movimiento = self.vecinos[ciudad_destino]
+            movimiento.peso_mx = peso
+            movimiento.precio = precio
+        else:
+            movimiento = Movimiento(self.id, ciudad_destino, peso, precio)
+            self.vecinos[ciudad_destino] = movimiento
 
     def obtener_ponderacion(self, ciudad_destino, tipo='precio'):
         if ciudad_destino in self.vecinos:
             movimiento = self.vecinos[ciudad_destino]
             return movimiento.precio if tipo == 'precio' else movimiento.peso_mx
-        return float('inf') if tipo == 'precio' else float('inf')  # Cambiado de float('-inf')
+        return float('inf')
+    
+    def __str__(self):
+        return str(self.id) + ' conectadoA: ' + str([x for x in self.vecinos.keys()])
+
+    def obtener_conexiones(self):
+        return self.vecinos.keys()
 
     def __lt__(self, otro_vertice):
         return self.distancia_precio < otro_vertice.distancia_precio
@@ -79,59 +81,122 @@ class ColaPrioridad:
                     self.lista[i] = (nueva_distancia, vertice)
                     self.lista.sort(key=lambda x: x[0])
 
-def dijkstra(un_grafo, inicio, tipo='precio', destino=None):
+def dijkstra_precio_minimo(un_grafo, inicio, destino=None):
     cp = ColaPrioridad()
-
-    if tipo == 'precio':
-        inicio.distancia_precio = 0
-        cp.insertar((inicio.distancia_precio, inicio))
-    elif tipo == 'peso':
-        inicio.distancia_peso = 0
-        cp.insertar((inicio.distancia_peso, inicio))
+    inicio.distancia_precio = 0
+    cp.insertar((inicio.distancia_precio, inicio))
 
     while not cp.esta_vacia():
-        if tipo == 'precio':
-            distancia, vertice_actual = cp.eliminar_min()
-        elif tipo == 'peso':
-            distancia, vertice_actual = cp.eliminar_min()
+        distancia, vertice_actual = cp.eliminar_min()
+
         for ciudad_destino in vertice_actual.obtener_conexiones():
             vertice_siguiente = un_grafo.vertices[ciudad_destino]
-            if tipo == 'precio':
-                nueva_distancia = distancia + vertice_actual.obtener_ponderacion(ciudad_destino, 'precio')
-                if nueva_distancia < vertice_siguiente.distancia_precio:
-                    vertice_siguiente.distancia_precio = nueva_distancia
-                    vertice_siguiente.predecesor_precio = vertice_actual
-                    cp.decrementar_clave(vertice_siguiente, nueva_distancia)
-            elif tipo == 'peso':
-                nueva_distancia = distancia + vertice_actual.obtener_ponderacion(ciudad_destino, 'peso')
-                if nueva_distancia < vertice_siguiente.distancia_peso:
-                    vertice_siguiente.distancia_peso = nueva_distancia
-                    vertice_siguiente.predecesor_peso = vertice_actual
-                    cp.decrementar_clave(vertice_siguiente, nueva_distancia)
+            nueva_distancia = distancia + vertice_actual.obtener_ponderacion(ciudad_destino, 'precio')
+            if nueva_distancia < vertice_siguiente.distancia_precio:
+                vertice_siguiente.distancia_precio = nueva_distancia
+                vertice_siguiente.predecesor_precio = vertice_actual
+                cp.decrementar_clave(vertice_siguiente, nueva_distancia)
+
+def dijkstra_peso_maximo(un_grafo, inicio, destino=None):
+    cp = ColaPrioridad()
+    inicio.distancia_peso = 0
+    cp.insertar((inicio.distancia_peso, inicio))
+
+    while not cp.esta_vacia():
+        distancia, vertice_actual = cp.eliminar_min()
+
+        for ciudad_destino in vertice_actual.obtener_conexiones():
+            vertice_siguiente = un_grafo.vertices[ciudad_destino]
+            nuevo_peso = max(distancia, vertice_actual.obtener_ponderacion(ciudad_destino, 'peso'))
+            if nuevo_peso < vertice_siguiente.distancia_peso:
+                vertice_siguiente.distancia_peso = nuevo_peso
+                vertice_siguiente.predecesor_peso = vertice_actual
+                cp.decrementar_clave(vertice_siguiente, nuevo_peso)
 
 def encontrar_peso_precio_minimo(un_grafo, ciudad_origen, ciudad_destino=None):
     resultados = {}
 
-    dijkstra(un_grafo, un_grafo.vertices[ciudad_origen], 'precio', ciudad_destino)
-    dijkstra(un_grafo, un_grafo.vertices[ciudad_origen], 'peso', ciudad_destino)
+    # Reiniciar distancias y predecesores antes de cada búsqueda
+    for vertice in un_grafo.vertices.values():
+        vertice.distancia_precio = float('inf')
+        vertice.distancia_peso = float('inf')
+        vertice.predecesor_precio = None
+        vertice.predecesor_peso = None
+
+    # Búsqueda de precio mínimo
+    dijkstra_precio_minimo(un_grafo, un_grafo.vertices[ciudad_origen], ciudad_destino)
 
     for ciudad_dest, vertice_dest in un_grafo.vertices.items():
         if ciudad_dest != ciudad_origen:
             predecesor_precio = vertice_dest.predecesor_precio
-            cuello_botella_precio = vertice_dest.obtener_ponderacion(predecesor_precio, 'precio') if predecesor_precio else None
             precio_min = vertice_dest.distancia_precio
-            predecesor_peso = vertice_dest.predecesor_peso
-            cuello_botella_peso = vertice_dest.obtener_ponderacion(predecesor_peso, 'peso') if predecesor_peso else None
-            peso_max = vertice_dest.distancia_peso
+            cuello_botella_precio = None
+
+            if predecesor_precio:
+                cuello_botella_precio = vertice_dest.obtener_ponderacion(predecesor_precio.id, 'precio')
 
             resultados[ciudad_dest] = {'precio_min': precio_min, 'cuello_botella_precio': cuello_botella_precio,
-                                       'peso_max': peso_max, 'cuello_botella_peso': cuello_botella_peso}
+                                       'peso_max': None, 'cuello_botella_peso': None}
+
+    # Reiniciar distancias y predecesores antes de la búsqueda de peso máximo
+    for vertice in un_grafo.vertices.values():
+        vertice.distancia_peso = float('inf')
+        vertice.predecesor_peso = None
+
+    # Búsqueda de peso máximo
+    dijkstra_peso_maximo(un_grafo, un_grafo.vertices[ciudad_origen], ciudad_destino)
+
+    for ciudad_dest, vertice_dest in un_grafo.vertices.items():
+        if ciudad_dest != ciudad_origen:
+            predecesor_peso = vertice_dest.predecesor_peso
+            peso_max = vertice_dest.distancia_peso
+            cuello_botella_peso = None
+
+            if predecesor_peso:
+                cuello_botella_peso = vertice_dest.obtener_ponderacion(predecesor_peso.id, 'peso')
+
+            resultados[ciudad_dest]['peso_max'] = peso_max
+            resultados[ciudad_dest]['cuello_botella_peso'] = cuello_botella_peso
 
     return resultados
 
+
+    # Búsqueda de precio mínimo
+def dijkstra_precio_minimo(un_grafo, inicio, destino=None):
+    cp = ColaPrioridad()
+    inicio.distancia_precio = 0
+    cp.insertar((inicio.distancia_precio, inicio))
+
+    while not cp.esta_vacia():
+        distancia, vertice_actual = cp.eliminar_min()
+
+        for ciudad_destino in vertice_actual.obtener_conexiones():
+            vertice_siguiente = un_grafo.vertices[ciudad_destino]
+            nueva_distancia = distancia + vertice_actual.obtener_ponderacion(ciudad_destino, 'precio')
+            if nueva_distancia < vertice_siguiente.distancia_precio:
+                vertice_siguiente.distancia_precio = nueva_distancia
+                vertice_siguiente.predecesor_precio = vertice_actual
+                cp.decrementar_clave(vertice_siguiente, nueva_distancia)
+
+def dijkstra_peso_maximo(un_grafo, inicio, destino=None):
+    cp = ColaPrioridad()
+    inicio.distancia_peso = 0
+    cp.insertar((inicio.distancia_peso, inicio))
+
+    while not cp.esta_vacia():
+        distancia, vertice_actual = cp.eliminar_min()
+
+        for ciudad_destino in vertice_actual.obtener_conexiones():
+            vertice_siguiente = un_grafo.vertices[ciudad_destino]
+            nueva_distancia = max(distancia, vertice_actual.obtener_ponderacion(ciudad_destino, 'peso'))
+            if nueva_distancia < vertice_siguiente.distancia_peso:
+                vertice_siguiente.distancia_peso = nueva_distancia
+                vertice_siguiente.predecesor_peso = vertice_actual
+                cp.decrementar_clave(vertice_siguiente, nueva_distancia)
+
 # Crear el grafo y cargar datos desde el archivo
 grafo = Grafo()
-ruta_del_archivo = os.path.abspath("C:\Users\Fernando\Desktop\Problema 3/rutas.txt")  # Ruta relativa
+ruta_del_archivo = os.path.abspath("/home/dany/Escritorio/Nueva carpeta/ejerciciosTP2/ejercicio3/rutas.txt")  # Reemplazar con la ruta correcta
 
 try:
     with open(ruta_del_archivo, 'r') as arch_ruta:
